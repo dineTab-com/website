7db3d8b53ab3252109340196d6a401bd42cc4353
Chris Soueidan <chris@loogart.com>
2021-01-20 14:38:28 -0500

    testing lottie
    lottie with absolute path test

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,30 @@
 AOS.init();
 
-var animation1 = bodymovin.loadAnimation({
-    container: document.getElementById('bm1'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'js/jumbotron.json'
-})
 var animation2 = bodymovin.loadAnimation({
     container: document.getElementById('bm2'),
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: 'js/foodies.json'
+    path: 'https://dinetab.com/js/foodies.json'
 })
 var animation3 = bodymovin.loadAnimation({
     container: document.getElementById('bm3'),
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: 'js/restos.json'
+    path: 'https://dinetab.com/js/restos.json'
 })
 var animation4 = bodymovin.loadAnimation({
     container: document.getElementById('bm4'),
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: 'js/servers.json'
+    path: 'https://dinetab.com/js/servers.json'
 })
 var animation5 = bodymovin.loadAnimation({
     container: document.getElementById('bm5'),
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: 'js/mobile.json'
+    path: 'https://dinetab.com/js/mobile.json'
 })
